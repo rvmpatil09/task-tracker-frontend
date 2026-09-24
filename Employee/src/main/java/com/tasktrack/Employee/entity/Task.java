c26a9fe932c33190f2a4f9fc6feb65049b94a099
RPGitProjects <rvmpatil09@gmail.com>
2026-09-24 14:27:35 +0530

    Added createdBy, location and createdAt fields

--- Employee/src/main/java/com/tasktrack/Employee/entity/Task.java
+++ Employee/src/main/java/com/tasktrack/Employee/entity/Task.java
@@ -13,5 +13,11 @@ public class Task {
 
     private String title;
     private String description;
-    private String status = "Pending";
+    private boolean completed;
+
+    private String createdBy;
+    private String location;
+    private LocalDateTime createdAt;
+
+
 }
